ca1648fd8afbcb0408fc38750c43003f2294efcd
Alfredo Alea <alfredoalea@gmail.com>
2025-12-14 14:05:37 -0500

    Update home page message with welcoming content

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 export default function HomePage() {
   return (
-    <h1>This is a temporary Home page for Alfredo Alea ML Articles</h1>
+    <h3>This a placeholder for Alfredo Alea ML Articles Blog page. Feel free to post any comments on the three concepts of Machine Learning I have illustrated in the ML Articles link above. Sign up using your email and a password to post comments and upvotes for each article. Thank you for visiting my page</h3>
   );
 }
